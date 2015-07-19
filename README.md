9f0492639282587db4fb34357cfd19b2ffdcd760
Wasin Thonkaew <haxpor@gmail.com>
2015-07-19 21:38:51 +0700

    Add archive, export, build for simulator, and build then launching simulator. Update README.md. Cleaned up comments in script.

--- README.md
+++ README.md
@@ -6,3 +6,35 @@ Works, but mostly right now it uses fixed setting in the script. Thus change tho
 I will be working on it more for more flexible and general in use of the script. But it's not so soon.
 
 I'm around on twitter, hit me @haxpor.
+
+Commands
+===
+It involves using the following programs. Thus at this point, make sure you have them all installed in your system.
+
+1. xcodebuild
+2. ios-deploy
+3. ios-sim
+4. lldb
+
+Commands support (*specific for this script and project)
+
+* `zhbuild build`  
+	Build Debug-build
+	
+* `zhbuild build-sim`  
+	Build a project for simulator.
+
+* `zhbuild clean`  
+	Clean the whole project
+
+* `zhbuild xcconfig`  
+    Create an xcconfig file for Debug configuration of 'ZombieHero - Fast Build' scheme
+
+* `zhbuild debug ipad`  
+    Build, deploy and debug Debug-build on ipad device. Build process is the same as in 1.
+
+* `zhbuild debug sim`
+	Build, and launching simulator.
+
+* `zhbuild debug`  
+    Build, deploy and debug Debug-build on ipod touch device. Build process is the same as in 1.
--- zhbuild.sh
+++ zhbuild.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
 ## A simple shell script to build Zombie Hero Project from command line
-## It involves using the following programs
-##
-## 1. xcodebuild
-## 2. ios-deploy
-##
-## Commands support (*specific for this script and project)
-##
-## 1. zhxcode build
-##    Build Debug-build    
-##
-## 2. zhxcode clean
-##    Clean the whole project
-##
-## 3. zhxcode xcconfig
-##    Create an xcconfig file for Debug configuration of 'ZombieHero - Fast Build' scheme
-##
-## 4. zhxcode debug ipad
-##    Build, deploy and debug Debug-build on ipad device. Build process is the same as in 1.
-##
-## 5. zhxcode debug
-##    Build, deploy and debug Debug-build on ipod touch device. Build process is the same as in 1.
+## See README.md
 
 # setting for Zombie Hero Project
 WPATH="/Users/haxpor/Data/Projects/ZombieHero/zombie-hero/"
 FASTDEBUG_XCCONFIG_PATH="../xcconfig/zh-fastbuild.xcconfig"
+FASTDEBUG_SIM_XCCONFIG_PATH="../xcconfig/zh-fastbuild-sim.xcconfig"
 XCODEPROJ_NAME="ZombieHero.xcodeproj"
 PRODUCT_PATH="/Users/haxpor/Library/Developer/Xcode/DerivedData/ZombieHero-erdynsruzgedcidywngbikwfyczm/Build/Products/Debug-iphoneos/Zombie\ Hero\ -\ Fast\ Build.app"
+PRODUCT_NAME="Zombie Hero - Fast Build"
+ARCHIVE_PATH="~/Desktop/ZombieHero.xarchive"
+EXPORT_PATH="~/Desktop/ZombieHero.ipa"
+PROVISIONING_PROFILE_NAME="10ffa923-c4cc-4cb6-82e1-503ce585d53e"
+SIMULATOR_DEVICETYPEID="iPhone-6-Plus, 8.4"
+IOS_SIM_TMP_LOG="/tmp/ios-sim-log"
 
 # check the command
 
@@ -36,6 +23,21 @@ if [ "$1" == "build" ]; then
 
     # build Zombie Hero (fixed and default to 'Zombie Hero - Fast Build' for now
     (cd $WPATH && exec xcodebuild -project $XCODEPROJ_NAME -xcconfig $FASTDEBUG_XCCONFIG_PATH -scheme "ZombieHero - Fast Build" -configuration Debug -destination "platform=iOS,name=iPod touch" -verbose)
+
+# 'build-sim'
+elif [ "$1" == "build-sim" ]; then
+    # build Zombie Hero for ios simulator
+    (cd $WPATH && exec xcodebuild -project $XCODEPROJ_NAME -xcconfig $FASTDEBUG_SIM_XCCONFIG_PATH -scheme "ZombieHero - Fast Build" -configuration Debug -destination "platform=iphonesimulator,OS=8.4,id=3CD97526-5E6A-4670-A89F-D68A446BDF66" -verbose)
+
+# 'archive'
+elif [ "$1" == "archive" ]; then
+    # archive the proejct
+    (cd $WPATH && exec xcodebuild -project $XCODEPROJ_NAME -scheme "ZombieHero Beta" archivePath $ARCHIVE_PATH archive -verbose)
+
+# 'export'
+elif [ "$1" == "export" ]; then
+    # export into .ipa file from archive resulted of 'archive' command
+    (cd $WPATH && exec xcodebuild -exportArchive -archivePath $ARCHIVE_PATH.xcarchive -exportPath $EXPORT_PATH -exportFormat ipa -exportProvisioningProfile $PROVISIONING_PROFILE_NAME)
 
 # 'clean'
 elif [ "$1" == "clean" ]; then
@@ -68,6 +70,27 @@ elif [[ "$1" == "debug" ]] && [[ "$2" == "ipad" ]]; then
     # deploy
     # deploy to the fixed path; see path in xcconfig file of this project
     (ios-deploy --debug --bundle /Users/haxpor/Library/Developer/Xcode/DerivedData/ZombieHero-erdynsruzgedcidywngbikwfyczm/Build/Products/Debug-iphoneos/Zombie\ Hero\ -\ Fast\ Build.app -v -i 12052c824186f4dc4820a5712baeb835f7d85ff1)
+
+# 'debug' with simulator
+elif [[ "$1" == "debug" ]] && [[ "$2" == "sim" ]]; then
+    echo "Building onto simulator ..."
+    #buid the project first
+    (cd $WPATH && exec xcodebuild -project $XCODEPROJ_NAME -xcconfig $FASTDEBUG_SIM_XCCONFIG_PATH -scheme "ZombieHero - Fast Build" -configuration Debug -destination "platform=iphonesimulator,OS=8.4,id=3CD97526-5E6A-4670-A89F-D68A446BDF66" -verbose)
+
+    # Check exit status
+    if [ $? != 0 ]; then
+        echo -e "Build failed.\nExit now.\n"
+
+        # exit with the last command status
+        exit $?
+    fi
+
+    echo "Remove the previous log ..."
+    rm -rf $IOS_SIM_TMP_LOG
+    echo "Running simulator with log ..."
+    ios-sim launch /Users/haxpor/Library/Developer/Xcode/DerivedData/ZombieHero-erdynsruzgedcidywngbikwfyczm/Build/Products/Debug-iphonesimulator/Zombie\ Hero\ -\ Fast\ Build.app --devicetypeid $SIMULATOR_DEVICETYPEID --log ./.ios-sim-log &
+    echo "Launching lldb and wait for the process to attach ..."
+    lldb -n "$PRODUCT_NAME" -w
 
 # 'debug'
 elif [ "$1" == "debug" ]; then
